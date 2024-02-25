aabd0937a4ed08469e8c51afdc17ec322ea76f85
jakub walczak <jakubwalczak@op.pl>
2024-02-25 07:44:46 +0100

    Add a function that returns a string

--- cards/main.go
+++ cards/main.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+var deckSize int
+
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
