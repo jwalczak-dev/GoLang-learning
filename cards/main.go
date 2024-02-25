5e10db3b2b44f440a5bf4013bb80f8b7e85e76d7
jakub walczak <jakubwalczak@op.pl>
2024-02-25 11:23:21 +0100

    Adding new file with type 'deck'

--- cards/main.go
+++ cards/main.go
@@ -5,9 +5,17 @@ import "fmt"
 var deckSize int
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
-	printState()
+	// card := newCard()
+	// fmt.Println(card)
+	// printState()
+
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	// fmt.Println(cards)
+
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
 }
 
 func newCard() string {
