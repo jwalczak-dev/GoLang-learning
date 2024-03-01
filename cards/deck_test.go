cda37965b7531f823de53f57092813a318f49872
jakub walczak <jakubwalczak@op.pl>
2024-03-01 16:56:10 +0100

    Adding another test for IO operations check

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,18 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_dectesting")
+
+	deck := newDeck()
+	deck.saveToFile("_dectesting")
+
+	loadedDeck := newDeckFromFile("_dectesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
 }
