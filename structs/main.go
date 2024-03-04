d8ff9ec33aba0b04f414e67d080b26fd37d3abd0
jakub walczak <jakubwalczak@op.pl>
2024-03-04 20:13:53 +0100

    Adding more examples with structs

--- structs/main.go
+++ structs/main.go
@@ -2,22 +2,49 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	alex := person{"Alex", "Anderson"}
+	// alex := person{"Alex", "Anderson"}
 	// bob := person{firstName: "Bob", lastName: "Sponge"}
 	var joe person
 
-	fmt.Println(alex)
+	// fmt.Println(alex)
 	fmt.Println(joe)
 	fmt.Printf("%+v\n", joe)
 
 	joe.firstName = "Joe"
 	joe.lastName = "Doe"
 	fmt.Printf("%+v\n", joe)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.details()
+
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) details() {
+	fmt.Printf("%+v\n", p)
 
 }
