cd23aaf4ba2220da6b21ebd4c875852b1c69993b
jakub walczak <jakubwalczak@op.pl>
2024-03-05 22:12:32 +0100

    Adding recap with pointers and value/reference types

--- map/main.go
+++ map/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+type colorMapType map[string]string
+
 func main() {
 	// var colors map[string]string
 
@@ -11,18 +13,34 @@ func main() {
 	// colors["black"] = "#000000"
 	// delete(colors, "white")
 
-	colors := map[string]string{
+	colors := colorMapType{
 		"red":   "ff0000",
 		"green": "#4bf745",
 		"white": "#ffffff",
 	}
 
+	//map type is a reference type, so it does not needs pointers to mutate
+	colors.addColor("black", "#000000")
 	printMap(colors)
 
+	//Only recap with pointers
+	//string type is a value type, so it needs pointers to mutate
+	test := "Teststring"
+	changeText(&test)
+	fmt.Println(test)
+
+}
+
+func (m colorMapType) addColor(color string, hex string) {
+	m[color] = hex
 }
 
 func printMap(c map[string]string) {
 	for color, hex := range c {
 		fmt.Printf("%v color has hex number %v\n", color, hex)
 	}
+}
+
+func changeText(s *string) {
+	(*s) = (*s) + "!!!"
 }
