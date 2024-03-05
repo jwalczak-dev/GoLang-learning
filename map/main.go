194c6a7ee10d606ad5582ab58fde1db12965b6dc
jakub walczak <jakubwalczak@op.pl>
2024-03-05 21:06:17 +0100

    Adding examples for manipulating maps

--- map/main.go
+++ map/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "ff0000",
-		"green": "#4bf745",
-	}
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	colors["white"] = "#ffffff"
+	colors["black"] = "#000000"
+	delete(colors, "white")
+
+	// colors := map[string]string{
+	// 	"red":   "ff0000",
+	// 	"green": "#4bf745",
+	// }
 
 	fmt.Println(colors)
 
